25f9851bcdeff15cf7ebb1d154cd7b48ca9916b8
ThanhTN <trannhatthanh31@gmail.com>
2019-10-02 14:42:20 +0700

    change picture to another one and add function move up down and left

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
  * Created by nhatnk on 4/26/17.
  */
 
-function Hero(image, top, left, size){
+function Hero(image, top, left, size, speed){
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
+  this.speed = speed;
 
   this.getHeroElement = function(){
     return '<img width="'+ this.size + '"' +
@@ -16,17 +17,29 @@ function Hero(image, top, left, size){
   }
 
   this.moveRight = function(){
-    this.left += 20;
+    this.left += speed;
     console.log('ok: ' + this.left);
+  }
+  this.moveLeft = function () {
+    this.left -= speed;
+  }
+  this.moveUp = function () {
+    this.top -= speed;
+  }
+  this.moveDown = function () {
+    this.top += speed;
+  }
+  this.speedUp = function () {
+    this.speed += 10;
   }
 
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+var hero = new Hero('Pepegon.jpg', 20, 30, 200, 30);
 
 function start(){
   if(hero.left < window.innerWidth - hero.size){
-    hero.moveRight();
+    hero.moveDown();
   }
   document.getElementById('game').innerHTML = hero.getHeroElement();
   setTimeout(start, 500)
